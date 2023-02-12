37de2909a1347d996bb17a8bca709e748f22c411
Shibo Jie <parsley@pku.edu.cn>
2023-02-13 01:00:25 +0800

    Update run.sh

--- FacT/run.sh
+++ FacT/run.sh
@@ -5,5 +5,5 @@ for DATASET in cifar caltech101 dtd oxford_flowers102 oxford_iiit_pet svhn sun39
 
 for DATASET in cifar caltech101 dtd oxford_flowers102 oxford_iiit_pet svhn sun397 patch_camelyon eurosat resisc45 diabetic_retinopathy clevr_count clevr_dist dmlab kitti dsprites_loc dsprites_ori smallnorb_azi smallnorb_ele
     do
-        CUDA_VISIBLE_DEVICES=0 python fact_tt.py --dataset $DATASET
+        CUDA_VISIBLE_DEVICES=0 python fact_tk.py --dataset $DATASET
     done
